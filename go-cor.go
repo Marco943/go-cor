751a74ec23df03bb5dfa5a242a3f72cb395dceaf
Marco <macto.14@gmail.com>
2024-02-27 21:36:34 -0300

    lista de cores

--- go-cor.go
+++ go-cor.go
@@ -14,6 +14,8 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
+var cores = []string{"ffffff", "ff0000", "ff00ff", "00ff00", "bb7700", "77dd00", "928274", "ffffff", "ff0000", "ff00ff", "00ff00", "bb7700", "77dd00", "928274"}
+
 func main() {
 	app := app.New()
 	janela := app.NewWindow("Go-Cor")
@@ -21,15 +23,17 @@ func main() {
 
 	corAtual := canvas.NewText("", color.White)
 	rectCorAtual := canvas.NewRectangle(color.NRGBA{255, 255, 255, 255})
+	rectCorAtual.CornerRadius = 10
 	caixaCorAtual := container.NewStack(rectCorAtual, corAtual)
 
-	containerCoresSalvas := container.NewGridWithColumns(1,
-		widget.NewLabel("Cores Salvas"),
-		container.NewStack(
-			widget.NewButton("#ff0000", func() {}),
-			canvas.NewRectangle(color.NRGBA{R: 255, G: 0, B: 0, A: 255}),
-		),
-	)
+	listaCores := container.NewVBox(widget.NewLabel("Cores Salvas"))
+
+	for i := range cores {
+		cor := colorBox(cores[i])
+		listaCores.Add(&cor)
+	}
+
+	containerCoresSalvas := container.NewScroll(listaCores)
 
 	content := container.NewGridWithColumns(2, containerCoresSalvas, caixaCorAtual)
 	janela.SetContent(content)
@@ -44,8 +48,18 @@ func main() {
 	encerrar()
 }
 
-func HexRGB(hex string) color.NRGBA {
-	values, _ := strconv.ParseUint(string(hex), 16, 32)
+func colorBox(corHex string) fyne.Container {
+	corRGB := HexRGB(&corHex)
+	corFonte := corContraste(&corRGB)
+	texto := canvas.NewText(fmt.Sprintf("#%v", corHex), corFonte)
+	rect := canvas.NewRectangle(corRGB)
+	rect.CornerRadius = 5
+	rect.SetMinSize(fyne.NewSize(20, 30))
+	return *container.NewStack(rect, container.NewCenter(texto))
+}
+
+func HexRGB(hex *string) color.NRGBA {
+	values, _ := strconv.ParseUint(string(*hex), 16, 32)
 	return color.NRGBA{R: uint8(values >> 16), G: uint8((values >> 8) & 0xFF), B: uint8(values & 0xFF), A: 255}
 }
 
@@ -60,7 +74,7 @@ func corContraste(cor *color.NRGBA) color.Color {
 func atualizarCor(cor *canvas.Text, rectCor *canvas.Rectangle) {
 	x, y := robotgo.Location()
 	corHex := robotgo.GetPixelColor(x, y)
-	corRGB := HexRGB(corHex)
+	corRGB := HexRGB(&corHex)
 
 	cor.Text = fmt.Sprintf("#%v", corHex)
 	cor.Color = corContraste(&corRGB)
